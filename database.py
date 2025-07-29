ebd0c7895f290030575ae7e9f2d524a886577958
AntonioCar0lin0 <antoniorpcarolino@gmail.com>
2025-07-29 11:10:28 -0300

    feat/adicionando sslmode para sqlalchemy

--- database.py
+++ database.py
@@ -10,7 +10,7 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    DB_DIALECT = os.getenv("DB_DIALECT", "postgres")
+    DB_DIALECT = os.getenv("DB_DIALECT", "postgresql")
     DB_USER = os.getenv("DB_USER")
     DB_PASSWORD = os.getenv("DB_PASSWORD")
     DB_HOST = os.getenv("DB_HOST", "localhost")
@@ -18,10 +18,13 @@ if not DATABASE_URL:
     DB_NAME = os.getenv("DB_NAME")
 
     if not all([DB_USER, DB_PASSWORD, DB_NAME]):
-        raise ValueError(" Variáveis de banco de dados ausentes no .env!")
+        raise ValueError("❌ Variáveis de banco de dados ausentes no .env!")
 
-    DATABASE_URL = f"{DB_DIALECT}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    # Adiciona sslmode=require no final da string
+    DATABASE_URL = (
+        f"{DB_DIALECT}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
+    )
 
-# Cria o engine
+# Cria o engine com a URL final
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
