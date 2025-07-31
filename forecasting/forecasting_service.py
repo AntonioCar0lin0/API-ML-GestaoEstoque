37607e8de7bf6e4877b64f666c9ef38b56ffc748
anton <antoniorpcarolino@gmail.com>
2025-07-31 15:09:52 -0300

    feat: fortalecendo a conexao das sessoes do bd

--- forecasting/forecasting_service.py
+++ forecasting/forecasting_service.py
@@ -1,17 +1,22 @@
-# Carrega os dados de transação e gera as previsões 
+# forecasting_service.py
 import pandas as pd
-from database import engine
+from sqlalchemy import text
+from database import SessionLocal
 from forecasting.model_selector import selecionar_melhor_modelo
 
 def carregar_dados_transacao(tipo: str = None):
     query = "SELECT data, valor FROM transacoes"
     if tipo in ["receita", "despesa"]:
         query += f" WHERE tipo = '{tipo}'"
-    
-    df = pd.read_sql(query, engine)
-    df['data'] = pd.to_datetime(df['data'])
-    df = df.groupby('data').sum().asfreq('D').fillna(0)
-    return df
+
+    db = SessionLocal()
+    try:
+        df = pd.read_sql(text(query), db.bind)  #  usa o bind da sessão
+        df['data'] = pd.to_datetime(df['data'])
+        df = df.groupby('data').sum().asfreq('D').fillna(0)
+        return df
+    finally:
+        db.close()
 
 def prever(modelo, periodo: int = 30):
     previsao = modelo.predict(n_periods=periodo)
